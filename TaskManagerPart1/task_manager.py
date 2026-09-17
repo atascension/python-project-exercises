7c71a10c403ce657d892c73d8c84b460a7eae552
Robert Morgan <220232196+atascension@users.noreply.github.com>
2026-09-16 21:29:50 -0400

    first commit

---
+++ TaskManagerPart1/task_manager.py
@@ -0,0 +1,11 @@
+print("Welcome to the Python Task Manager!") 
+print("---------------------------")
+print("1. View tasks")
+print("2. Add a task")
+print("3. Complete a task")
+print("4. Remove a task")
+print("5. Exit\n")
+
+user_option = int(input("Choose an option (1-5): "))
+
+print(f"You selected option: {user_option} ")
